2e8aa1ca06b7181bd74912745884f6c42cb72bb9
figurec <86502121+figurec@users.noreply.github.com>
2021-06-27 02:43:10 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,23 +1,18 @@
-import socket
-
 PORT = int(os.environ(['PORT']))
 
-serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-serv_sock.bind(('', PORT))
-serv_sock.listen(10)
+import socket
+
+sock = socket.socket()
+sock.bind(('', PORT))
+sock.listen(1)
+conn, addr = sock.accept()
+
+print 'connected:', addr
 
 while True:
-    # Бесконечно обрабатываем входящие подключения
-    client_sock, client_addr = serv_sock.accept()
-    print('Connected by', client_addr)
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
 
-    while True:
-        # Пока клиент не отключился, читаем передаваемые
-        # им данные и отправляем их обратно
-        data = client_sock.recv(1024)
-        if not data:
-            # Клиент отключился
-            break
-        client_sock.sendall(data)
-
-    client_sock.close()
+conn.close()
