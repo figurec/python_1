8ffb0ff053feea3d31fbcbe455785969bdf91590
figurec <86502121+figurec@users.noreply.github.com>
2021-06-27 04:55:57 +0300

    Update start.py

--- start.py
+++ start.py
@@ -4,6 +4,16 @@ import socket
 port = int(os.environ.get("PORT", 5000))
 print("PORT: ", port)
 
+def read(client_sock):
+    buffer = ''
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            # Клиент отключился
+            break
+        buffer += data
+    return buffer
+
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 serv_sock.bind(('', port))
 serv_sock.listen(10)
@@ -16,11 +26,12 @@ while True:
     while True:
         # Пока клиент не отключился, читаем передаваемые
         # им данные и отправляем их обратно
-        data = client_sock.recv(1024)
-        if not data:
+        #data = client_sock.recv(1024)
+        #if not data:
             # Клиент отключился
-            client_sock.close()
-            break
+            #client_sock.close()
+            #break
+        data = read(client_sock)
         print(data)
         if data[0] == 10:
             result = b'this work'
