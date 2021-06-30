a0b5f6c85143664f70b020e70b30f08fb86fb6c1
figurec <86502121+figurec@users.noreply.github.com>
2021-06-30 05:14:35 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import selectors
+
 sel = selectors.DefaultSelector()
 
 PORT = int(os.environ.get('PORT', 5000))
@@ -7,55 +8,62 @@ print("PORT: ", PORT)
 
 import socket
 
+'''
 def server():
-  serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-  serv_sock.bind(('', 53210))
-  serv_sock.listen(10)
-
-  while True:
-    # Бесконечно обрабатываем входящие подключения
-    client_sock, client_addr = serv_sock.accept()
-    print('Connected by', client_addr)
+    serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+    serv_sock.bind(('', 53210))
+    serv_sock.listen(10)
 
     while True:
-        # Пока клиент не отключился, читаем передаваемые
-        # им данные и отправляем их обратно
-        data = client_sock.recv(1024)
-        if not data:
-            # Клиент отключился
-            break
-        client_sock.sendall(b"data")
+        # Бесконечно обрабатываем входящие подключения
+        client_sock, client_addr = serv_sock.accept()
+        print('Connected by', client_addr)
 
-    client_sock.close()
+        while True:
+            # Пока клиент не отключился, читаем передаваемые
+            # им данные и отправляем их обратно
+            data = client_sock.recv(1024)
+            if not data:
+                # Клиент отключился
+                break
+            client_sock.sendall(b"data")
+
+        client_sock.close()
+'''
 
 def read(sock):
-  buffer = b""
-  while True:
-    data = sock.recv(1000)
-    if not data:
-      break
-    buffer += data
-  return buffer
-  #print(data) 
+    buffer = b""
+    while True:
+        data = sock.recv(1024 * 50)
+        if not data:
+            break
+        buffer += data
+    return buffer
+    #print(data)
+
 
 def client():
-  opt = ("example.com", 80)
-  cln = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  #cln = socket.create_connection(opt);
-  cln.connect(opt)
-  h = b"GET / HTTP/1.1\r\n"
-  h += b"Host: example.com\r\n\r\n"
-  cln.sendall(h)
-  res = read(cln)
-  print(res)
-  cln.close
+    opt = ("example.com", 80)
+    cln = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #cln = socket.create_connection(opt);
+    cln.connect(opt)
+    h = b"GET / HTTP/1.1\r\n"
+    h += b"Host: example.com\r\n\r\n"
+    cln.sendall(h)
+    res = read(cln)
+    print(res)
+    cln.close
+
+
+import time
 
 
 def main():
-  print("main")
-  #socket.create_connection
-  #exit(0)
-  client()
+    print("main")
+    #exit(0)
+    start = time.time()
+    client()
+    print ("it took", time.time() - start, "seconds.")
 
 if __name__ == "__main__":
-  main()
+    main()
