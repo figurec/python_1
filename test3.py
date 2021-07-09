99f2f89fad2e75cc40bc66b07ee1c4c726cc9042
figurec <86502121+figurec@users.noreply.github.com>
2021-07-09 16:57:01 +0300

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,19 @@
+import socket
+import os
+
+HOST = ''
+PORT = int(os.environ.get("PORT", 5000))
+print("PORT: ", PORT)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+sock.bind((HOST, PORT))
+sock.listen(1)
+while True:
+  conn, addr = sock.accept()
+  while True:
+    data = conn.recv(1024)
+    if data:
+      print(data)
+    else:
+      conn.close()
+      break
